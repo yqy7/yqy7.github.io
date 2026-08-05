fc0e31a65587a9678d5badea75e638dd15d00faa
yqy7 <qiyun.yqy@gmail.com>
2026-08-06 01:02:26 +0800

    feat: 添加 Google Analytics 统计 (gtag + SPA 路由追踪)

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,20 @@
-import { Outlet } from "react-router"
+import { useEffect } from "react"
+import { Outlet, useLocation } from "react-router"
 import { Navbar } from "@/components/Navbar"
 
+declare global {
+  interface Window {
+    gtag?: (...args: unknown[]) => void
+  }
+}
+
 export function Layout() {
+  const location = useLocation()
+
+  useEffect(() => {
+    window.gtag?.("config", "G-BE93K4DNXC", { page_path: location.pathname })
+  }, [location])
+
   return (
     <>
       <Navbar />
